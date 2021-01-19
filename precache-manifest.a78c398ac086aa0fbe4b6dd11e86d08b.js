afdd883c76f26776beb9ef04f152c83c70a7239a
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2021-01-19 09:41:31 -0800

    Updates

---
+++ precache-manifest.a78c398ac086aa0fbe4b6dd11e86d08b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "837c946bc44a3a1dc1d4c99f685d3db4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "478af6b93842d6aa3134",
+    "url": "/portfolio/static/css/main.302acada.chunk.css"
+  },
+  {
+    "revision": "c1e45827dfe8a3f5409f",
+    "url": "/portfolio/static/js/2.9ce490ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9ce490ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "478af6b93842d6aa3134",
+    "url": "/portfolio/static/js/main.80bbfb56.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "dadcd6c7a17f86c58e29a33ba0dc0b6a",
+    "url": "/portfolio/static/media/Headshot-1.dadcd6c7.jpg"
+  }
+]);
--- src/redux/root-reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import portfolioReducer from './portfolio/portfolio.reducer'
-
-export default combineReducers({
-    portfolio: portfolioReducer
-});
