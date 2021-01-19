4df2aa1b214740cf89c380b4860cec9bf3006977
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2021-01-19 10:06:17 -0800

    backing up main code

--- src/redux/portfolio/portfolio.data.js
+++ src/redux/portfolio/portfolio.data.js
@@ -1,9 +1,16 @@
+import brfLogo from './images/blackraven-collage.jpg'
+import crownLogo from './images/crown-clothing.jpg'
+import cssLogo from './images/gradient-logo.jpg'
+import ricksLogo from './images/rickslawn.jpg'
+import roboLogo from './images/robofriends-logo.jpg'
+import issLogo from './images/telescope-logo.jpg'
+
 const PORTFOLIO_DATA = [
     {
         title: 'Crown Clothing',
         subtitle: 'React.js, Redux, Stripe API, Firebase',
         type: 'code',
-        imageUrl: '/assets/crown-clothing.jpg',
+        imageUrl: crownLogo,
         codeUrl: 'https://github.com/rebeccachavezlv/clothing-store',
         webUrl: 'https://rc-crown-live.herokuapp.com',
         id: 1
@@ -12,7 +19,7 @@ const PORTFOLIO_DATA = [
         title: 'RoboFriends',
         subtitle: 'React.js, RESTful API',
         type: 'code',
-        imageUrl: '/assets/robofriends-logo.jpg',
+        imageUrl: roboLogo,
         codeUrl: 'https://github.com/rebeccachavezlv/robofriends/tree/master',
         webUrl: 'https://rebeccachavezlv.github.io/robofriends/',
         id: 2
@@ -21,7 +28,7 @@ const PORTFOLIO_DATA = [
         title: 'Black Raven Films',
         subtitle: 'WordPress, CSS',
         type: 'code',
-        imageUrl: '/assets/blackraven-collage.jpg',
+        imageUrl: brfLogo,
         codeUrl: null,
         webUrl: 'https://www.blackravenfilms.com',
         id: 3
@@ -30,7 +37,7 @@ const PORTFOLIO_DATA = [
         title: 'ISS Finder',
         subtitle: 'JavaScript, RESTful API',
         type: 'code',
-        imageUrl: '/assets/telescope-logo.jpg',
+        imageUrl: issLogo,
         codeUrl: 'https://github.com/rebeccachavezlv/ISSFinder',
         webUrl: 'https://rebeccachavezlv.github.io/ISSFinder/',
         id: 4
@@ -39,7 +46,7 @@ const PORTFOLIO_DATA = [
         title: "Rick's Lawn and Landscaping",
         subtitle: 'WordPress, CSS',
         type: 'code',
-        imageUrl: '/assets/rickslawn.jpg',
+        imageUrl: ricksLogo,
         codeUrl: null,
         webUrl: 'https://www.rickslawnandlandscaping-lv.com',
         id: 5
@@ -48,7 +55,7 @@ const PORTFOLIO_DATA = [
         title: 'CSS Gradient Generator',
         subtitle: 'HTML, CSS, JavaScript',
         type: 'code',
-        imageUrl: '/assets/gradient-logo.jpg',
+        imageUrl: cssLogo,
         codeUrl: 'https://github.com/rebeccachavezlv/cssbackgroundgenerator',
         webUrl: 'https://rebeccachavezlv.github.io/cssbackgroundgenerator/',
         id: 6
