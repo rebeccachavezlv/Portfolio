5383d30fc07cac94653ad3089ef0191e34238b3c
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2021-01-20 09:41:08 -0800

    added hover effect to portfolio backgrounds

--- src/Components/portfolio/gallery.component.jsx
+++ src/Components/portfolio/gallery.component.jsx
@@ -16,17 +16,20 @@ class GalleryItem extends React.Component{
 
 
         return(
-            <div className='galleryItem' style={{ backgroundImage: `url(${this.props.imageUrl})`}}>
-                <div className='galleryTitles'>
-                    <h2>{this.props.title}</h2>
-                    <h3>{this.props.subtitle}</h3>
-                </div>
-                <div className='galleryButtons'>
-                    <a href={this.props.webUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Live'/></a>
-                    {this.props.codeUrl !== null &&
-                        <a href={this.props.codeUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Code'/></a>
-                    }
-                </div>
+            <div className='galleryItem' >
+                <div className='bg-image' style={{ backgroundImage: `url(${this.props.imageUrl})`}}></div>
+                <div className='gallery-info'>
+                    <div className='gallery-titles'>
+                        <h2 className='title-text'>{this.props.title}</h2>
+                        <h3 className='title-text'>{this.props.subtitle}</h3>
+                    </div>
+                    <div className='galleryButtons'>
+                        <a href={this.props.webUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Live'/></a>
+                        {this.props.codeUrl !== null &&
+                            <a href={this.props.codeUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Code'/></a>
+                        }
+                    </div>
+                    </div>
             </div>
         )
     }
