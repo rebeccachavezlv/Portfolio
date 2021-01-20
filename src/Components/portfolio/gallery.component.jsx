7e95e4dcceccb13182151bdd1f7c6da344ed1512
RebeccaChavezLV <rebeccachavezlv@gmail.com>
2021-01-20 09:46:57 -0800

    converted to functional component, destructured props for readability

--- src/Components/portfolio/gallery.component.jsx
+++ src/Components/portfolio/gallery.component.jsx
@@ -5,34 +5,22 @@ import ButtonComponent from '../button/button.component'
 
 import './gallery.styles.scss'
 
-
-
-class GalleryItem extends React.Component{
-    constructor(props){
-        super()
-    }
-
-    render() {
-
-
-        return(
-            <div className='galleryItem' >
-                <div className='bg-image' style={{ backgroundImage: `url(${this.props.imageUrl})`}}></div>
-                <div className='gallery-info'>
-                    <div className='gallery-titles'>
-                        <h2 className='title-text'>{this.props.title}</h2>
-                        <h3 className='title-text'>{this.props.subtitle}</h3>
-                    </div>
-                    <div className='galleryButtons'>
-                        <a href={this.props.webUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Live'/></a>
-                        {this.props.codeUrl !== null &&
-                            <a href={this.props.codeUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Code'/></a>
-                        }
-                    </div>
-                    </div>
+const GalleryItem = ({imageUrl, title, subtitle, webUrl, codeUrl}) => (
+    <div className='galleryItem' >
+        <div className='bg-image' style={{ backgroundImage: `url(${imageUrl})`}}></div>
+        <div className='gallery-info'>
+            <div className='gallery-titles'>
+                <h2 className='title-text'>{title}</h2>
+                <h3 className='title-text'>{subtitle}</h3>
             </div>
-        )
-    }
-}
+            <div className='galleryButtons'>
+                <a href={webUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Live'/></a>
+                {codeUrl !== null &&
+                    <a href={codeUrl} target='_blank' rel="noopener noreferrer"><ButtonComponent title='Code'/></a>
+                }
+            </div>
+            </div>
+    </div>
+)
 
 export default GalleryItem
